250fc58bf2f50465b412ed26ae41a021cea1b618
AirBaker55 <abesarami@ieschandomonte.edu.es>
2024-10-18 14:21:20 +0200

    2.2º

--- dam1-abr/src/ud2/ejemplos/Factura.java
+++ dam1-abr/src/ud2/ejemplos/Factura.java
@@ -1,8 +1,21 @@
 package ud2.ejemplos;
-
+import java.util.*;
 public class Factura {
     public static void main(String[] args) {
         System.out.println("Introduce el precio y la cantidad de producto");
+        Scanner sc = new Scanner(System.in);
+        double preciso = sc.nextDouble();
+        double cantidad = sc.nextDouble();
+        double total = preciso*cantidad;
+        double IVa= total*0.21;
+        double totalIva= total+IVa;
+        double preciodescuento=  totalIva - (totalIva * 0.05) ;
+        if (totalIva >= 100) {
+            System.out.println("Tienes que pagar: "+preciodescuento+" EUROS");
+        }
+        else{
+            System.out.println("Tienes que pagar: "+totalIva+" EUROS");
+        }
 
     }
 }

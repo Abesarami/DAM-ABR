d7817097c7cedfc06c8e489906a11b22d8be4c1b
AirBaker55 <abesarami@ieschandomonte.edu.es>
2024-10-18 13:57:58 +0200

    2.1º

---
+++ dam1-abr/src/ud2/ejemplos/Factura.java
@@ -0,0 +1,8 @@
+package ud2.ejemplos;
+
+public class Factura {
+    public static void main(String[] args) {
+        System.out.println("Introduce el precio y la cantidad de producto");
+
+    }
+}

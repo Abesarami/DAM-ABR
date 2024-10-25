cc3df4c07c4f21cae59909b7d9015ca0d3c66c3d
AirBaker55 <abesarami@ieschandomonte.edu.es>
2024-10-25 14:15:41 +0200

    Create EP0213.java

---
+++ dam1-abr/src/ud2/ejemplos/EP0213.java
@@ -0,0 +1,28 @@
+package ud2.ejemplos;
+
+import java.util.*;
+
+public class EP0213 {
+    public static void main(String[] args) {
+        System.out.println(
+                "En este programa calcularemos si la comida es suficiente para los animales y si no lo es diremos cuanta comida falta");
+
+        Scanner sc = new Scanner(System.in);
+
+        double cantidacomida, cantidadAnimales, consumoAnimal, cantidadsobrante;
+
+        System.out.println("Introduce la cantidad de animales");
+        cantidadAnimales = sc.nextDouble();
+        System.out.println("Introduce la cantidad de comida");
+        cantidacomida = sc.nextDouble();
+        System.out.println("Introduce lo que consumen de comida cada animal");
+        consumoAnimal = sc.nextDouble();
+        
+
+        cantidadsobrante = cantidacomida - cantidadAnimales*consumoAnimal;
+        if (cantidacomida>(cantidadAnimales*consumoAnimal)) {
+            System.out.println("Tienes comida suficiente y te sobraran: "+ cantidadsobrante);
+            
+        }
+    }
+}

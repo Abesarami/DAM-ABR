946110a830224c3345c70e59ce94cb3680c2b39a
AirBaker55 <abesarami@ieschandomonte.edu.es>
2024-11-06 14:19:51 +0100

    8.2

--- dam1-abr/src/ud2/Util.java
+++ dam1-abr/src/ud2/Util.java
@@ -1,12 +1,31 @@
 package ud2;
 
+
 public class Util {
+    
     
 
     static boolean esPar(int n) {
-        return n / 2 == 0;
+        return n % 2 == 0;
        
 
     }
-      
+
+    static int mayor(int a, int   b ){
+        int mayor =  a > b ? a : b;
+        return mayor; 
+
+    }
+
+    static boolean bisiesto (int n){
+        if (n%4==0 || n%100 == 0 && n%400==0) {
+            return true;
+            
+        }
+        else{
+            return false;
+        }
+            
+    }
+    
 }

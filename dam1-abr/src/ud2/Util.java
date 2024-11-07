033a5e9d141acd1f2a8e0189c953a28f9cce99d5
AirBaker55 <abesarami@ieschandomonte.edu.es>
2024-11-07 12:13:17 +0100

    9.3

--- dam1-abr/src/ud2/Util.java
+++ dam1-abr/src/ud2/Util.java
@@ -1,31 +1,68 @@
 package ud2;
 
+import java.util.Scanner;
 
 public class Util {
-    
-    
 
-    static boolean esPar(int n) {
+    public  static boolean esPar(int n) {
         return n % 2 == 0;
-       
 
     }
 
-    static int mayor(int a, int   b ){
-        int mayor =  a > b ? a : b;
-        return mayor; 
+    public  static int mayor(int a, int b) {
+        int mayor = a > b ? a : b;
+        return mayor;
 
     }
 
-    static boolean bisiesto (int n){
-        if (n%4==0 || n%100 == 0 && n%400==0) {
+    public static boolean bisiesto(int n) {
+        if (n % 4 == 0 || n % 100 == 0 && n % 400 == 0) {
             return true;
-            
-        }
-        else{
+
+        } else {
             return false;
         }
-            
+
     }
-    
+
+    public static boolean casiCero(double n) {
+        if (n > -1 && n < 1 && n != 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static String notatexto(double n) {
+        String nota;
+        if (n < 5) {
+            nota = "Insuficiente";
+
+        } else if (n < 6) {
+            nota = "Suficiente";
+
+        } else if (n < 7) {
+            nota = "bien";
+
+        } else if (n < 9) {
+            nota = "Notable";
+
+        } else if (n <= 10) {
+            nota = "sobresaliente";
+        } else {
+            nota = "nota incorrecta";
+        }
+
+        return nota;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+
+        System.out.println(notatexto(n));
+    }
+
 }

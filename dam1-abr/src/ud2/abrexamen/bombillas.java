3321d799d60e34720f6b999e01149169ce64f8f1
AirBaker55 <abesarami@ieschandomonte.edu.es>
2024-11-21 12:54:14 +0100

    ¿?¿?¿

--- dam1-abr/src/ud2/abrexamen/bombillas.java
+++ dam1-abr/src/ud2/abrexamen/bombillas.java
@@ -1,4 +1,8 @@
 package ud2.abrexamen;
+// Antonio Besada Ramilo
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
 
 public class bombillas {
     public static void main(String[] args) {
@@ -9,20 +13,37 @@ public class bombillas {
         int horasMuerte = Encendidos * TiempoEncendido;
         String Resultado = "";
 
-        if (TiempoEncendido >= 1 && TiempoEncendido <= 10 && Encendidos > 0) {
+        if (TiempoEncendido >= 1 && TiempoEncendido <= 10 && Encendidos > 0 && Horas>0 ) {
             if (Horas < horasMuerte) {
                 Resultado = "HORAS";
 
-            } else if (Horas > horasMuerte) {
+            } else if (Horas > horasMuerte) {   
                 Resultado = "ENCENDIDOS";
 
             } else if (Horas == horasMuerte) {
                 Resultado = "ENCENDIDOS + HORAS";
             }
         } else {
-            Resultado = "Error";
+            Resultado = "ERROR";
         }
 
         return Resultado;
     }
+    @Test
+    public void causaFinBombillaTest() {
+    assertEquals("ERROR", causaFinBombilla(100, 100, 100));
+    assertEquals("ERROR", causaFinBombilla(1000, -10, 10));
+    assertEquals("ERROR", causaFinBombilla(0, 10, 10));
+    assertEquals("ERROR", causaFinBombilla(1000, 10, 0));
+    assertEquals("ENCENDIDOS", causaFinBombilla(500, 400, 1));
+    assertEquals("HORAS", causaFinBombilla(1000, 10000, 1));
+    assertEquals("HORAS", causaFinBombilla(1000, 200, 10));
+    assertEquals("HORAS", causaFinBombilla(2000, 700, 3));
+    assertEquals("ENCENDIDOS", causaFinBombilla(2000, 600, 3));
+    assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 1));
+    assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 9));
+    assertEquals("ENCENDIDOS + HORAS", causaFinBombilla(1000, 100, 10));
+    }
+    
+    
 }
--- dam1-abr/src/ud2/abrexamen/chicles.java
+++ dam1-abr/src/ud2/abrexamen/chicles.java
@@ -1,20 +1,24 @@
 package ud2.abrexamen;
+//Antonio Besada Ramilo
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
 
 public class chicles {
     public static void main(String[] args) {
-        System.out.println(totalChicles(20, 2, 5));
+        System.out.println(totalChicles(100, 2, 4));
     }
 
-    public static int totalChicles(int chiclesCompra, int EnvoltoriosNecesarios, int Regalo) {
+    public static int totalChicles(int chiclesCompra, int envoltoriosNecesarios, int Regalo) {
         int chiclesRegalo, chicles;
 
-        if (EnvoltoriosNecesarios != 0) {
-            if (Regalo < EnvoltoriosNecesarios && EnvoltoriosNecesarios > 0) {
-                chiclesRegalo = chiclesCompra / EnvoltoriosNecesarios * Regalo;// 5
+        if (envoltoriosNecesarios != 0) {
+            if (Regalo < envoltoriosNecesarios && envoltoriosNecesarios > 0) {
+                chiclesRegalo = chiclesCompra / envoltoriosNecesarios * Regalo;// 5
 
                 chicles = chiclesCompra + chiclesRegalo;// 30
-                if (chiclesRegalo >= EnvoltoriosNecesarios) {
-                    int chiclesExtra = chiclesRegalo / EnvoltoriosNecesarios * Regalo;
+                if (chiclesRegalo >= envoltoriosNecesarios) {
+                    int chiclesExtra = chiclesRegalo / envoltoriosNecesarios * Regalo;
                     chicles = chicles + chiclesExtra;
                 }
 
@@ -26,6 +30,14 @@ public class chicles {
         }
 
         return chicles;
-
     }
-}
+        @Test
+        public void ChiclesRegaloTest() {
+        assertEquals(31, totalChicles(25, 5, 1));
+        assertEquals(6, totalChicles(5, 5, 1 ));
+        assertEquals(111, totalChicles(100, 10, 1));
+        assertEquals(100, totalChicles(100, 0, 0));
+        assertEquals(-1, totalChicles(20, 2, 5));
+        assertEquals(-1, totalChicles(10, -1, 0));
+        }
+    }

0e6f62ce565da25ebcd326404478a4dba026bdf8
AirBaker55 <abesarami@ieschandomonte.edu.es>
2025-01-14 13:50:54 +0100

    ?=?¿?¿?¿

---
+++ dam1-abr/src/ud4/apuntes/E0511.java
@@ -0,0 +1,8 @@
+package ud4.apuntes;
+
+public class E0511 {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al juego de la camara secreta");
+        System.out.println( "En esta aplicacion tendras que adivinar la combinación para una camara secreta ");
+    }
+}
--- dam1-abr/src/ud4/ejercicios10012025/EP0512_Desordenar.java
+++ dam1-abr/src/ud4/ejercicios10012025/EP0512_Desordenar.java
@@ -1,18 +1,33 @@
 package ud4.ejercicios10012025;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
+
+import static ud4.ArraysUtil.arrayAleatorio;
+
 public class EP0512_Desordenar {
-    void desordenar(int t[]){
+    public static void main(String[] args) {
+       int t[] = arrayAleatorio(6,1,10);
+        Arrays.sort(t);
+        System.out.println(Arrays.toString(t));
+       desordenar(t);
+        System.out.println(Arrays.toString(t));
+    }
+    
+    public static void desordenar(int t[]){
          Random rnd = new Random();
-           int t1[] = Arrays.copyOf(t, t.length);
-            
-           
-           /*
-             * ahora hay que cambiar el orden del array se podria hacer cambiando la posicion de cada elemento del array por otra aleatoria
-             * pero no soy capaz 
-             */
+        for (int i = 0; i < t.length; i++) {
+            int nuevaPosicion = rnd.nextInt(t.length);
+            if (nuevaPosicion != i){
+                int aux = t[i];
+                t[i]= t[nuevaPosicion];
+                t[nuevaPosicion]=aux;
+            }
+        }
+
+
             
 
     }

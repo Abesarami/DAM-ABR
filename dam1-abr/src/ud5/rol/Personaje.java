ae17d58bcb083e86041a7c7378e42d613839dfe0
AirBaker55 <abesarami@ieschandomonte.edu.es>
2025-02-19 14:26:01 +0100

    3434

--- dam1-abr/src/ud5/rol/Personaje.java
+++ dam1-abr/src/ud5/rol/Personaje.java
@@ -13,10 +13,12 @@ private RAZAS raza;
 private String nombre;
 private int nivel;
 private int experiencia;
+private int puntosTot;
 private int puntos;
 
+
 public Personaje(int fuerza, int agilidad, int constitucion, RAZAS raza, String nombre, int nivel,
-        int experiencia, int puntos) {
+        int experiencia, int puntosTot) {
     this.fuerza = fuerza;
     this.agilidad = agilidad;
     this.constitucion = constitucion;
@@ -24,7 +26,8 @@ public Personaje(int fuerza, int agilidad, int constitucion, RAZAS raza, String 
     this.nombre = nombre;
     this.nivel = nivel;
     this.experiencia = experiencia;
-    this.puntos = puntos;
+    this.puntosTot = puntosTot;
+    this.puntos = puntosTot;
 }
 
 public Personaje(int fuerza, int agilidad, int constitucion, RAZAS raza, String nombre) {
@@ -33,14 +36,55 @@ public Personaje(int fuerza, int agilidad, int constitucion, RAZAS raza, String 
     this.constitucion = constitucion;
     this.raza = raza;
     this.nombre = nombre;
+    this.nivel = 1;
+    this.experiencia = 0;
+    this.puntosTot = constitucion+50;
+    this.puntos = puntosTot;
 }
+
 
 public Personaje(String nombre) {
     this.nombre = nombre;
     this.raza = RAZAS.HUMANO;
-    
+    this.nivel = 1;
+    this.experiencia = 0;
+    this.fuerza =(int) Math.random()*100+1;
+    this.agilidad =(int) Math.random()*100+1;
+    this.constitucion =(int) Math.random()*100+1;
+    this.puntosTot = constitucion+50;
+    this.puntos = puntosTot;
+
 }
+public Personaje(String nombre ,RAZAS raza) {
+    this.nombre = nombre;
+    this.raza = raza;
+    this.nivel = 1;
+    this.experiencia = 0;
+    this.fuerza =(int) Math.random()*100+1;
+    this.agilidad =(int) Math.random()*100+1;
+    this.constitucion =(int) Math.random()*100+1;
+    this.puntosTot = constitucion+50;
+    this.puntos = puntosTot;
+
+}
+   public void mostrar(){
+    System.out.println(nombre);
+    System.out.println(puntosTot + "hp");
+    System.out.println(raza);
+    System.out.println(nivel);
+    System.out.println(experiencia);
+    System.out.println(fuerza + "fuerza");
+    System.out.println(agilidad + "agilidad");
+    System.out.println(constitucion + "constitucion");
     
+
+   } 
+   @Override
+   public String toString() {
+       System.out.println(nombre);
+       System.out.println(puntos+"/"+puntosTot);
+           return nombre;
+   }
 
     
 }

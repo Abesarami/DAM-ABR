56d5bb83d3bc666beab85ff33501f705dc080344
AirBaker55 <abesarami@ieschandomonte.edu.es>
2025-04-25 14:27:15 +0200

    clase meigas

--- dam1-abr/src/UD6/FestivalDasMeigas/Meiga.java
+++ dam1-abr/src/UD6/FestivalDasMeigas/Meiga.java
@@ -1,18 +1,38 @@
 package UD6.FestivalDasMeigas;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.Set;
+
+import repaso.repaso;
 
 public class Meiga {
     private String Nome = "";
     private String AlcumeMaxicos = "";
-    //    private Feitizo[] FeitizosFav;
-    private Set<Feitizo> FeitizosFav;
+    // private Feitizo[] FeitizosFav;
+    private Set<Feitizo> Feitizos;
+    private Map<String, Integer> inventario;
 
-    public Meiga(String nome, String alcumeMaxicos, Set<Feitizo> feitizosFav) {
-        Nome = nome;
-        AlcumeMaxicos = alcumeMaxicos;
-        FeitizosFav = feitizosFav;
+    public Meiga(String nome, String alcumeMaxicos, Collection<Feitizo> feitizos, Map<String, Integer> inventario) {
+            Nome = nome;
+            AlcumeMaxicos = alcumeMaxicos;
+            Feitizos = Feitizos;
+        this.inventario = inventario;
+    }
+
+    public void setFeitizos(Set<Feitizo> feitizos) {
+        Feitizos = feitizos;
+    }
+
+    public Map<String, Integer> getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(Map<String, Integer> inventario) {
+        this.inventario = inventario;
     }
 
     public Meiga(String nome, String alcumeMaxicos) {
@@ -37,36 +57,79 @@ public class Meiga {
         AlcumeMaxicos = alcumeMaxicos;
     }
 
-    public Set<Feitizo> getFeitizosFav() {
-        return FeitizosFav;
+    public Set<Feitizo> getFeitizos() {
+        return Feitizos;
     }
 
-    public void setFeitizosFav(Set<Feitizo> feitizosFav) {
-        FeitizosFav = feitizosFav;
+
+    //metodos
+    private static Collection<Feitizo> getFeitizosRnd(Collection<Feitizo> feitizosExemplo) {
+        Random rnd = new Random();
+        for (int i = 0; i < 5; i++) {
+           int aleat= rnd.nextInt(Feitizo.crearFeitizosExemplo().length-0+1);
+            Collection<Feitizo> Feitizos = Feitizos.add(Feitizo.crearFeitizosExemplo()[aleat]);
+        }
+                return Feitizos;
     }
+
+    private static Map<String, Integer> getIngredientesRnd(Collection<String> ingredientesExemplo, int i) {
+    }
+
+    public static Meiga[] crearMeigasExemplo(Feitizo[] feitizosDisponibles) {
+        Random rnd = new Random();
+
+        String[] nomes = { "Uxía", "Lúa", "Iria", "Noa" };
+        String[] alcumes = { "A das Fraguiñas", "Sombra do Vento", "Luz do Bosque", "Meiga da Brétema" };
+
+        Meiga[] meigas = new Meiga[nomes.length];
+
+        // Obtén todos os feitizos de exemplo e todos os seus ingredientes únicos
+        Collection<Feitizo> feitizosExemplo = List.of(Feitizo.crearFeitizosExemplo());
+        Collection<String> ingredientesExemplo = Feitizo.ingredientesUnicos(feitizosExemplo);
+
+        for (int i = 0; i < nomes.length; i++) {
+            String nome = nomes[i];
+            String alcume = alcumes[i];
+            // Xera unha colección cunha selección aleatoria de feitizos de exemplo para
+            // asignar á meiga
+            Collection<Feitizo> feitizos = getFeitizosRnd(feitizosExemplo);
+                        // Xera un mapa cunha selección aleatoria de ingredientes e cantidades de cada
+                        // un de exemplo para asignar á meiga
+                        Map<String, Integer> inventario = getIngredientesRnd(ingredientesExemplo, 3);
+                                    
+                                                // Crea a meiga e a engade ao array
+                                                meigas[i] = new Meiga(nome, alcume, feitizos, inventario);
+                                            }
+                                    
+                                            return meigas;
+                                    
+                                        }
+                        
+                           
+               
 
 }
 
-//    public Meiga(String nome, String alcumeMáxicos) {
-//        Nome = nome;
-//        AlcumeMáxicos = alcumeMáxicos;
-//        FeitizosFav = new Feitizo[0];
-//    }
+// public Meiga(String nome, String alcumeMáxicos) {
+// Nome = nome;
+// AlcumeMáxicos = alcumeMáxicos;
+// FeitizosFav = new Feitizo[0];
+// }
 //
-//    public Meiga(String nome, String alcumeMáxicos, Feitizo[] feitizosFav) {
-//        Nome = nome;
-//        AlcumeMáxicos = alcumeMáxicos;
-//        FeitizosFav = feitizosFav;
-//    }
+// public Meiga(String nome, String alcumeMáxicos, Feitizo[] feitizosFav) {
+// Nome = nome;
+// AlcumeMáxicos = alcumeMáxicos;
+// FeitizosFav = feitizosFav;
+// }
 //
-//    public boolean EngadirFeitizo(Feitizo f){
-//        for (int i = 0; i < FeitizosFav.length; i++) {
-//            if (FeitizosFav[i].equals(f)){
-//                return false;
-//            }
-//        }
-//        FeitizosFav = Arrays.copyOf(FeitizosFav , FeitizosFav.length);
-//        FeitizosFav[FeitizosFav.length-1] = f;
-//        return true;
-//    }
-//}
+// public boolean EngadirFeitizo(Feitizo f){
+// for (int i = 0; i < FeitizosFav.length; i++) {
+// if (FeitizosFav[i].equals(f)){
+// return false;
+// }
+// }
+// FeitizosFav = Arrays.copyOf(FeitizosFav , FeitizosFav.length);
+// FeitizosFav[FeitizosFav.length-1] = f;
+// return true;
+// }
+// }
